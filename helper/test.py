2a097680f7c7118c11d6234cb6de7939da286793
Lukas <lucidLuckylee@users.noreply.github.com>
2024-03-07 07:44:04 +0100

    Cleanup

--- helper/test.py
+++ helper/test.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 import os
 import subprocess
 import resource
@@ -5,6 +6,17 @@ import sys
 import time
 import argparse
 import pace2024_verifier.pace as pace
+
+
+SOLUTION_PATH = "output.txt"
+
+
+class Status(Enum):
+    OK = "OK"
+    PROCESS_ERROR = "PROCESS ERROR"
+    TIMEOUT = "TIMED OUT"
+    OUT_OF_MEMORY = "OUT OF MEMORY"
+    COMMAND_ERROR = "COMMAND FAILED"
 
 
 def limit_virtual_memory(mem_limit_gb=None):
@@ -20,70 +32,69 @@ def limit_virtual_memory(mem_limit_gb=None):
                        (virtual_memory_limit, virtual_memory_limit))
 
 
-def run_command_with_limit(cmd, input_file, output_file, timeout, mem_limit_gb=None):
+def run_command_with_limit(cmd, input_path, output_path, timeout, mem_limit_gb=None):
     """
     Run a command with a time limit and redirecting stdin from a file.
 
     cmd: string, the command to run
-    input_file: string, path to the file for stdin redirection
+    input_path: string, path to the file for stdin redirection
     timeout: int, time limit in seconds
 
-    Returns: tuple containing return code, execution time, stdout, stderr,
-             and a boolean indicating if the process was terminated due to a timeout
+    Returns: tuple containing return code, execution time, stderr,
+             and a Status Enum indicating the process status after termination
     """
 
     start_time = time.time()
     status = None
-    try:
-        cmd = cmd.split(" ")
-        with open(input_file, 'r') as f, open(output_file, 'w') as out, subprocess.Popen(cmd, stdin=f, stdout=out,
-                                                                                         stderr=subprocess.PIPE,
-                                                                                         preexec_fn=limit_virtual_memory(
-                                                                                             mem_limit_gb)) as process:
-            try:
-                return_code = process.wait(timeout)
-            except subprocess.TimeoutExpired:
-                process.terminate()
-                return_code = process.wait()
-            end_time = time.time()
-            stderr = process.stderr.read().decode('utf-8')
-            if return_code == 0:
-                status = "ok"
-            else:
-                status = "Program exited with non zero exit code"
+    cmd = cmd.split(" ")
+    pace_graph_file = open(input_path, 'r')
+    output_file = open(output_path, 'w')
 
-    except subprocess.TimeoutExpired:
-        # This block will be entered if the command times out
+    try:
+        process = subprocess.Popen(
+            cmd,
+            stdin=pace_graph_file,
+            stdout=output_file,
+            stderr=subprocess.PIPE,
+            preexec_fn=limit_virtual_memory(mem_limit_gb)
+        )
+
+        try:
+            return_code = process.wait(timeout)
+            if return_code == 0:
+                status = Status.OK
+            else:
+                status = Status.PROCESS_ERROR
+        except subprocess.TimeoutExpired:
+            process.terminate()
+            return_code = process.wait()
+            status = Status.TIMEOUT
         end_time = time.time()
-        stderr = ''
-        return_code = ''
-        status = "timeout"
+        stderr = process.stderr.read().decode('utf-8')
+
     except MemoryError as e:
         end_time = time.time()
         stderr = str(e)
         return_code = -1
-        status = "memory limit"
+        status = Status.OUT_OF_MEMORY
+
     except subprocess.CalledProcessError as e:
         # This block will be entered if the command fails
         end_time = time.time()
         stderr = e.stderr.decode('utf-8') if e.stderr else ''
         return_code = e.returncode
-        status = "error"
+        status = Status.COMMAND_ERROR
 
-    except Exception as e:
-        # This block will be entered if an unexpected exception occurs
-        end_time = time.time()
-        stderr = str(e)
-        return_code = -1
-        status = "error"
+    pace_graph_file.close()
+    output_file.close()
 
     return return_code, end_time - start_time, stderr, status
 
 
 def count_crossings(input_path, solution_str):
     order = pace.read_solution(solution_str)
-    g = pace.read_graph(input_path, order=order)
-    crossings = g.countcrossings_segtree()
+    pace_graph = pace.read_graph(input_path, order=order)
+    crossings = pace_graph.countcrossings_segtree()
     return crossings
 
 
@@ -107,30 +118,30 @@ def check_if_solution_is_valid(graph_path, solution_path):
                 vertex = int(line)
                 if vertex < fixed_size or vertex > free_size + fixed_size:
                     print(f"Vertex {vertex} is out of range")
-                    return None
+                    return
 
                 if vertex in current_used_nodes:
                     print(f"Vertex {vertex} is already used")
-                    return None
+                    return
 
                 current_used_nodes.add(vertex)
                 order.append(vertex - 1)
 
             except ValueError:
                 print(f"Can not parse vertex {i}: ", line)
-                return None
+                return
 
     if len(current_used_nodes) != free_size:
         print(
             f"Only {len(current_used_nodes)} nodes are used, but {free_size} are required")
-        return None
+        return
 
 
 def clean_output(output_path):
-    with open(output_path, 'r') as f:
-        lines = f.readlines()
+    with open(output_path, 'r') as output_file:
+        lines = output_file.readlines()
 
-    with open(output_path, 'w') as f:
+    with open(output_path, 'w') as output_file:
         for line in lines:
             line = line.split("#")[0].strip()
             if len(line) == 0:
@@ -139,8 +150,8 @@ def clean_output(output_path):
             if "Set parameter Username" in line or "Academic license - for non-commercial use only -" in line:
                 continue
 
-            f.write(line)
-            f.write("\n")
+            output_file.write(line)
+            output_file.write("\n")
 
 
 def main():
@@ -153,34 +164,33 @@ def main():
     parser.add_argument("--timelimit", type=int, default=300,
                         help="The time limit for each run in seconds. Default is 300.")
     parser.add_argument("--memlimit", type=int, default=4,
-                        help="The memory limit for each run in GB. Default is 8.")
+                        help="The memory limit for each run in GB. Default is 4.")
 
     args = parser.parse_args()
-
     base_path = args.base_path
     program = args.program
+    paths = os.listdir(base_path)
 
-    files = os.listdir(base_path)
     try:
-        files = sorted(files, key=lambda x: int(x.split('.')[0]))
+        paths = sorted(paths, key=lambda x: int(x.split('.')[0]))
     except ValueError:
         pass
-    print("file,exit_code,time,status,crossings")
-    for f in files:
-        if f.endswith(".gr"):
-            path = os.path.join(base_path, f)
-            solution_path = "output.txt"
-            return_code, time_delta, stderr, status = run_command_with_limit(program, path, solution_path,
-                                                                             args.timelimit,
-                                                                             mem_limit_gb=args.memlimit)
+
+    print(f"{'File':<16}{'Exit Code':<16}{'Time':<16}{'Status':<16}{'Crossings':<16}")
+
+    for path in paths:
+        if path.endswith(".gr"):
+            pace_graph_path = os.path.join(base_path, path)
+            process_result = run_command_with_limit(program, pace_graph_path, SOLUTION_PATH, args.timelimit, mem_limit_gb=args.memlimit)
+            return_code, time_delta, stderr, status = process_result
 
             if return_code == 0:
-                clean_output(solution_path)
-                check_if_solution_is_valid(path, solution_path)
-                crossing = count_crossings(path, solution_path)
-                print(f, return_code, time_delta, status, crossing, sep=",")
+                clean_output(SOLUTION_PATH)
+                check_if_solution_is_valid(pace_graph_path, SOLUTION_PATH)
+                crossing = count_crossings(pace_graph_path, SOLUTION_PATH)
+                print(f"{path:<16}{return_code:<16}{time_delta:<16.5f}{status.value:<16}{crossing:<16}")
             else:
-                print(f, return_code, time_delta, status, "", sep=",")
+                print(f"{path:<16}{return_code:<16}{time_delta:<16.5f}{status.value:<16}{'':<16}")
             sys.stdout.flush()
 
 
