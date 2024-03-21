a7774abcba3559cd536e5c63b54b016ffb11af36
Jesse Palarus <jtsharkjtshark@gmail.com>
2024-03-21 12:51:41 +0100

    init ub in solver

--- src/pace_graph/solver.hpp
+++ src/pace_graph/solver.hpp
@@ -15,6 +15,7 @@ template <typename T> class Solver {
     std::chrono::time_point<std::chrono::steady_clock> start_time_for_part;
     std::chrono::milliseconds time_limit_for_part;
     bool reorderNodes;
+    bool initUB;
 
   protected:
     virtual void finish(PaceGraph &graph, std::vector<PaceGraph> &subgraphs,
@@ -24,19 +25,19 @@ template <typename T> class Solver {
 
   public:
     Solver(std::chrono::milliseconds limit = std::chrono::milliseconds::max(),
-           bool reorderNodes = false)
+           bool reorderNodes = false, bool initUB = true)
         : start_time(std::chrono::steady_clock::now()),
           start_time_for_part(std::chrono::steady_clock::now()),
           time_limit(limit),
           time_limit_for_part(std::chrono::milliseconds::zero()),
-          reorderNodes(reorderNodes) {}
+          reorderNodes(reorderNodes), initUB(initUB) {}
 
     void solve(PaceGraph &graph) {
         auto val = graph.splitGraphOn0Splits();
         auto splittedGraphs = std::get<0>(val);
         auto isolated_nodes = std::get<1>(val);
 
-        if (reorderNodes) {
+        if (reorderNodes || initUB) {
             MeanPositionParameter meanPositionParameter;
             MeanPositionSolver meanPositionSolver(
                 [this](int it) { return it == 0; }, meanPositionParameter);
@@ -44,68 +45,78 @@ template <typename T> class Solver {
             for (int i = 0; i < splittedGraphs.size(); ++i) {
                 auto g = splittedGraphs[i];
                 auto order = meanPositionSolver.solve(g);
-                splittedGraphs[i] = order.reorderGraph(g);
+                long ub = order.count_crossings(g);
+
+                if (reorderNodes) {
+                    splittedGraphs[i] = order.reorderGraph(g);
+                }
+
+                if (initUB) {
+                    splittedGraphs[i].ub = ub;
+                }
+            }
+
+            std::vector<T> results;
+            for (int i = 0; i < splittedGraphs.size(); i++) {
+                auto g = splittedGraphs[i];
+
+                start_time_for_part = std::chrono::steady_clock::now();
+
+                auto msLeft =
+                    time_limit -
+                    std::chrono::duration_cast<std::chrono::milliseconds>(
+                        start_time_for_part - start_time);
+
+                int sizeForAllUpcomingSegments = 0;
+                for (int j = i; j < splittedGraphs.size(); j++) {
+                    sizeForAllUpcomingSegments += splittedGraphs[j].size_free;
+                }
+
+                double percentageForThisSegment =
+                    static_cast<double>(g.size_free) /
+                    sizeForAllUpcomingSegments;
+
+                double newTimeLimitMs =
+                    msLeft.count() * percentageForThisSegment;
+
+                time_limit_for_part =
+                    std::chrono::milliseconds(static_cast<int>(newTimeLimitMs));
+
+                results.push_back(run(g));
+            }
+
+            finish(graph, splittedGraphs, results, isolated_nodes);
+        }
+
+        bool has_time_left() const {
+            return std::chrono::steady_clock::now() - start_time_for_part <
+                   time_limit_for_part;
+        }
+    };
+
+    class SolutionSolver : public Solver<Order> {
+      protected:
+        void finish(PaceGraph &graph, std::vector<PaceGraph> &subgraphs,
+                    std::vector<Order> &results,
+                    std::vector<int> &isolated_nodes) override {
+            for (int u : isolated_nodes) {
+                std::cout << graph.free_real_names[u] << std::endl;
+            }
+
+            for (int i = 0; i < subgraphs.size(); ++i) {
+                auto g = subgraphs[i];
+                auto sol = results[i];
+                std::cout << sol.convert_to_real_node_id(g) << std::endl;
             }
         }
 
-        std::vector<T> results;
-        for (int i = 0; i < splittedGraphs.size(); i++) {
-            auto g = splittedGraphs[i];
+        Order run(PaceGraph &graph) override = 0;
 
-            start_time_for_part = std::chrono::steady_clock::now();
-
-            auto msLeft = time_limit -
-                          std::chrono::duration_cast<std::chrono::milliseconds>(
-                              start_time_for_part - start_time);
-
-            int sizeForAllUpcomingSegments = 0;
-            for (int j = i; j < splittedGraphs.size(); j++) {
-                sizeForAllUpcomingSegments += splittedGraphs[j].size_free;
-            }
-
-            double percentageForThisSegment =
-                static_cast<double>(g.size_free) / sizeForAllUpcomingSegments;
-
-            double newTimeLimitMs = msLeft.count() * percentageForThisSegment;
-
-            time_limit_for_part =
-                std::chrono::milliseconds(static_cast<int>(newTimeLimitMs));
-
-            results.push_back(run(g));
-        }
-
-        finish(graph, splittedGraphs, results, isolated_nodes);
-    }
-
-    bool has_time_left() const {
-        return std::chrono::steady_clock::now() - start_time_for_part <
-               time_limit_for_part;
-    }
-};
-
-class SolutionSolver : public Solver<Order> {
-  protected:
-    void finish(PaceGraph &graph, std::vector<PaceGraph> &subgraphs,
-                std::vector<Order> &results,
-                std::vector<int> &isolated_nodes) override {
-        for (int u : isolated_nodes) {
-            std::cout << graph.free_real_names[u] << std::endl;
-        }
-
-        for (int i = 0; i < subgraphs.size(); ++i) {
-            auto g = subgraphs[i];
-            auto sol = results[i];
-            std::cout << sol.convert_to_real_node_id(g) << std::endl;
-        }
-    }
-
-    Order run(PaceGraph &graph) override = 0;
-
-  public:
-    explicit SolutionSolver(
-        std::chrono::milliseconds limit = std::chrono::milliseconds::max(),
-        bool reorderNodes = false)
-        : Solver<Order>(limit, reorderNodes) {}
-};
+      public:
+        explicit SolutionSolver(
+            std::chrono::milliseconds limit = std::chrono::milliseconds::max(),
+            bool reorderNodes = false)
+            : Solver<Order>(limit, reorderNodes) {}
+    };
 
 #endif // SOLVER_HPP
