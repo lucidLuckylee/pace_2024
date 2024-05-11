7697c47e6b61a76dbc9252a46b27c1411b26841e
Jesse Palarus <j.palarus@tu-berlin.de>
2024-05-11 17:20:44 +0200

    make exact code ready

--- make_submission.sh
+++ make_submission.sh
@@ -3,4 +3,8 @@
 rm -f heuristic_solver.tgz
 tar -czf heuristic_solver.tgz src CMakeLists.txt
 
+
+rm -f feedback_edge_set_solver.tgz
+tar -czf feedback_edge_set_solver.tgz src CMakeLists.txt
+
 echo "submissions created."
