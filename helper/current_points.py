8e4cd0eb77a5f1904023dcc4a11cbe1b9050f56b
Jesse Palarus <j.palarus@tu-berlin.de>
2024-05-25 01:43:53 +0200

    add code to get the current private rankings

--- helper/current_points.py
+++ helper/current_points.py
@@ -1,12 +1,14 @@
+import argparse
 import time
 
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-def main():
+def main(global_leaderboard, username, password):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
@@ -14,7 +16,18 @@ def main():
     service = ChromeService()
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    driver.get('https://www.optil.io/optilion/problem/3212#tab-4')
+    if global_leaderboard:
+        driver.get('https://www.optil.io/optilion/problem/3212#tab-4')
+    else:
+        driver.get('https://www.optil.io/optilion/login')
+        time.sleep(1)
+        driver.find_element(value="username").send_keys(username)
+        driver.find_element(value="password").send_keys(password)
+        driver.find_element(by=By.XPATH,
+                            value="/html/body/div[2]/section/div/div/div/div[2]/form/div[4]/div[1]/button").click()
+        time.sleep(5)
+        driver.get('https://www.optil.io/optilion/problem/3212#tab-3')
+
     time.sleep(5)
     rendered_html = driver.page_source
     driver.quit()
@@ -27,7 +40,13 @@ def main():
         elements = row.find_all('td')
         name = elements[0].get_text()
         points[name] = []
-        for score in elements[4:]:
+
+        if global_leaderboard:
+            elements = elements[4:]
+        else:
+            elements = elements[6:]
+
+        for score in elements:
             try:
                 points[name].append(int(score.get_text(strip=True).split('.')[0].replace(",", "")))
             except:
@@ -63,4 +82,13 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--global_leaderboard', type=bool,
+                        help='Weather to use the global leaderboard (leaderboard is not hidden) or only my runs')
+
+    parser.add_argument("--username", type=str, default="", help="Username to login (needed for my runs)")
+    parser.add_argument("--password", type=str, default="", help="Password to login (needed for my runs)")
+
+    args = parser.parse_args()
+
+    main(args.global_leaderboard, args.username, args.password)
